6c4be12ec4fdcc14fc455341be16793a8e75f08e
Olivia Maturana <oliviamaturanamcca@students.berkeley.net>
2019-01-15 14:26:26 -0800

    1/9/19

--- script.js
+++ script.js
@@ -21,16 +21,21 @@ function startGame(){
     document.getElementById("guesses").innerHTML = "";
     document.getElementById("hangFrame").innerHTML = "";
 
-    var rand = words[Math.floor(Math.random() * words.length)];
-    console.log(rand);
 
-    var level = documenrnt.getElementById("level").value;
+    var level = document.getElementById("level").value;
     if(level == 1){
         word = easyPeasy[Math.floor(Math.random() * easyPeasy.length)];
     }
     if(level == 2){
         word = kindaTricky[Math.floor(Math.random() * kindaTricky.length)];
     }
+    if(level == 3){
+        word = superTrippy[Math.floor(Math.random() * superTrippy.length)];
+    }
+    console.log(word);
+
+    document.getElementById("hangFrame").innerHTML = "<img src=img/8.png>";
+    printWord();
 
 
 
@@ -41,24 +46,33 @@ function printWord(){
     console.log(guessedLetters);
     var result = "";
 
-    if(word == guessedWord){
-        result = "Congrats! U R The Champion.";
-    }else{
-        for (var i = 0; i < word.length; i++){
-            if(guessedLetters.indexOf(word[i]) > -1){
-                result += word[i];
-            }else{
-                result += "_ ";
-            }
-        }
-        console.log(result);
-        if(guesses == 0){
-            result = "U LoSE AnD kIlLeD tHE GuY";
+
+    for (var i = 0; i < word.length; i++){
+        if(guessedLetters.indexOf(word[i]) > -1){
+            result += word[i];
+        }else{
+            result += "_ ";
         }
     }
+    console.log(result);
+
+    if(result.indexOf("_ ") == -1){
+        result = "Congrats! U R The Champion.";
+    }
+
+    if(guessedWord == word){
+        result = "Congrats! U R The Champion.";
+    }
+
+    console.log(result);
+    if(guesses == 0){
+        result = "U LoSE AnD kIlLeD tHE GuY";
+    }
+
 
     document.getElementById("wordSpaces").innerHTML = result;
     guessedWord = "";
+
 
 }
 
@@ -75,7 +89,7 @@ function handleGuess(){
         }
     }
 
-    if(guessedWord.length> 0 && numGuesses > 0 && guessedLetters.indexOf(guessedWord) == -1 && isNaN(parseInt(guessedWord))){
+    if(guessedWord.length> 0 && guesses > 0 && guessedLetters.indexOf(guessedWord) == -1 && isNaN(parseInt(guessedWord))){
         guessedLetters.push(guessedWord);
         if(guessedWord != word){
             guesses --;
